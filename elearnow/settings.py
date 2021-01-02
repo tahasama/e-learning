b54125d3b729c29c217c85bf056e1175cb7d5ef7
taha <taha.maatof@gmail.com>
2021-01-02 02:06:17 +0100

    push elearnow3437fadafsvfvi85

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
