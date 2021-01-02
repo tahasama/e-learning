e38d3f0fc905195352d3df987508af38cf8f7454
taha <taha.maatof@gmail.com>
2021-01-02 01:52:04 +0100

    push elearnow34376udgfdsfsvfvi85

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -156,6 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
@@ -167,8 +169,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 CACHES = {
     'default': {
