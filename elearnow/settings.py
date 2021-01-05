65a51a31c0505da97b1eda7be23b24459cd7ada4
taha <taha.maatof@gmail.com>
2021-01-05 09:49:10 +0100

    something00

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -111,6 +111,10 @@ DATABASES = {
         'PASSWORD': 'postgres',
     }
 }
+DATABASES['default'] = dj_database_url.config(default='')
+    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+    
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
