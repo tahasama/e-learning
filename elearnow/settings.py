95f431d0232bb1c92ba2f926709de66fa4a8011e
taha <taha.maatof@gmail.com>
2021-01-05 11:31:06 +0100

    something555

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
