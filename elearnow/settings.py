f4349ab51087654af59a3bc071e8a68460d5099b
taha <taha.maatof@gmail.com>
2021-01-05 13:00:16 +0100

    something444444444444433355

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -119,7 +119,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('redis', 6379)],
+            'hosts': [('elearnow.herokuapp.com', 6379)],
         },
     },
 }
