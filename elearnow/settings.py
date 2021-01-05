b6dad8bd60c7056f1ca693671500095c45c1836e
taha <taha.maatof@gmail.com>
2021-01-05 09:52:33 +0100

    something000

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -112,9 +112,9 @@ DATABASES = {
     }
 }
 DATABASES['default'] = dj_database_url.config(default='')
-    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'].update(db_from_env)
-    
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
