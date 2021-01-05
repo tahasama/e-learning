cde30bafedd6baf85f823cec22908fd3ad7f7c7d
taha <taha.maatof@gmail.com>
2021-01-05 18:05:41 +0100

    something11111

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -119,7 +119,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [('127.0.0.1', 6296)],
         },
     },
 }
