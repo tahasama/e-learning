845957112eab2f18f8beecda873e85b36f74aa65
taha <taha.maatof@gmail.com>
2021-01-05 11:15:33 +0100

    something777

--- elearnow/settings.py
+++ elearnow/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 # Application definition
